bb50e3e68322f1355be949a21195d6300aad3306
ArtemBoreyko <sreadbe44@gmail.com>
2020-02-03 17:33:45 +0200

    new project

---
+++ hello.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+	<title>hello</title>
+	</head>
+
+	<body>
+	Welcome, <?php echo $_POST["name"]; ?><br/>
+	Your age: <?php echo $_POST["age"]; ?><br/>
+	Your email: <?php echo $_POST["email"]; ?><br/>
+	</body>
+</html>
---
+++ test.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+	<title>Welcome!</title>
+	</head>
+	<body>
+	<p>
+	<form action="hello.php" method="post">
+	<p> Name: <input type="text" name="name"/></p>
+	<p> Age: <input type="text" name="age"/></p>
+	<p> Email: <input type="email" name="email"/></p>
+	<p><input type="submit" name="submit" value="Submit"/></p>
+	</form>
+	</p>
+	</body>
+<html>
